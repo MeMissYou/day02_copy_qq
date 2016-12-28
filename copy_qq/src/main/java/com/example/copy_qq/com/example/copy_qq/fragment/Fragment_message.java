2f657376eeafa13a04d61dc7977cb6e567825328
feng <1105517224@qq.com>
2016-12-28 16:11:29 +0800

    day_12_19

--- copy_qq/src/main/java/com/example/copy_qq/com/example/copy_qq/fragment/Fragment_message.java
+++ copy_qq/src/main/java/com/example/copy_qq/com/example/copy_qq/fragment/Fragment_message.java
@@ -2,10 +2,12 @@ package com.example.copy_qq.com.example.copy_qq.fragment;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,8 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -24,6 +28,7 @@ public class Fragment_message extends Fragment {
     ImageView iv;
     PopupWindow mpopupWindow;
     Button btn_popup;
+    Button btn_message,btn_tel;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
        Bundle savedInstanceState) {
@@ -34,13 +39,16 @@ public class Fragment_message extends Fragment {
         iv = (ImageView) v.findViewById(R.id.login_message);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.login);
         iv.setImageDrawable(new CicrleDrawable(bitmap));
+
         //popupWindow
         btn_popup = (Button) v.findViewById(R.id.btn_message_add);
         //创建一个popupwindow对象
         final View popupView = LayoutInflater.from(getContext()).inflate(R.layout.popupwindow,null);
-        mpopupWindow = new PopupWindow(
-                popupView, WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,true);
+        mpopupWindow = new PopupWindow(popupView);
+        mpopupWindow.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+        mpopupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+//                , WindowManager.LayoutParams.WRAP_CONTENT,
+//                WindowManager.LayoutParams.WRAP_CONTENT,true
         //使其聚齐
         mpopupWindow.setTouchable(true);
         //设置允许在外点击消失
@@ -49,50 +57,68 @@ public class Fragment_message extends Fragment {
         btn_popup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-             showPopupWindow();
+                mpopupWindow.showAsDropDown(btn_popup, Gravity.BOTTOM,60,0);
             }
         });
+        //top的消息和电话的点击按钮
+        btn_message = (Button) v.findViewById(R.id.btn_message_1);
+        btn_tel = (Button) v.findViewById(R.id.btn_message_2);
+//        btn_message.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                btn_message.setClickable(true);
+//                btn_tel.setClickable(false);
+//                        }
+//        });
+//        btn_tel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                btn_message.setClickable(false);
+//                btn_tel.setClickable(true);
+//            }
+//        });
 
-
+//        返回v
         return v;
       }
-    public  void showPopupWindow(){
-        Button btn1 = (Button) getView().findViewById(R.id.message_pop1);
-        Button btn2 = (Button) getView().findViewById(R.id.message_pop2);
-        Button btn3 = (Button) getView().findViewById(R.id.message_pop3);
-        Button btn4 = (Button) getView().findViewById(R.id.message_pop4);
-        Button btn5 = (Button) getView().findViewById(R.id.message_pop5);
-        mpopupWindow.showAsDropDown(btn_popup);
-    }
-    public void onClik (View view){
-        int id = view.getId();
-        switch (id){
-            case R.id.message_pop1:{
-                Toast.makeText(getContext(),"ha1 ",Toast.LENGTH_SHORT).show();
-                mpopupWindow.dismiss();
-            }
-            break;
-            case R.id.message_pop2:{
-                Toast.makeText(getContext(),"ha2 ",Toast.LENGTH_SHORT).show();
-                mpopupWindow.dismiss();
-            }
-            break;
-            case R.id.message_pop3:{
-                Toast.makeText(getContext(),"ha3 ",Toast.LENGTH_SHORT).show();
-                mpopupWindow.dismiss();
-            }
-            break;
-            case R.id.message_pop4:{
-                Toast.makeText(getContext(),"ha4 ",Toast.LENGTH_SHORT).show();
-                mpopupWindow.dismiss();
-            }
-            break;
-            case R.id.message_pop5:{
-                Toast.makeText(getContext(),"ha5 ",Toast.LENGTH_SHORT).show();
-                mpopupWindow.dismiss();
-            }
-            break;
-        }
-    }
+//    //子菜单的点击事件
+//    public  void showPopupWindow(){
+//        Button btn1 = (Button) getView().findViewById(R.id.message_pop1);
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getContext(),"点击了btn1",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        Button btn2 = (Button) getView().findViewById(R.id.message_pop2);
+//        btn2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getContext(),"点击了btn2",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        Button btn3 = (Button) getView().findViewById(R.id.message_pop3);
+//        btn3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getContext(),"点击了btn3",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        Button btn4 = (Button) getView().findViewById(R.id.message_pop4);
+//        btn4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getContext(),"点击了btn4",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        Button btn5 = (Button) getView().findViewById(R.id.message_pop5);
+//        btn5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getContext(),"点击了btn5",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+//    }
 
 }
